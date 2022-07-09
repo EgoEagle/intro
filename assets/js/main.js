4a2a453914e40dbebe64e0e18d2d1b1cb4479cb6
EgoEagle <74867858+EgoEagle@users.noreply.github.com>
2022-07-08 23:00:52 -0400

    day2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,65 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active2", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active2";
+}
+
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+
+
+function showSlides2(n) {
+	let i;
+	let slides = document.getElementsByClassName("mySlides2");
+	let dots = document.getElementsByClassName("dot2");
+	if (n > slides.length) {slideIndex2 = 1}    
+	if (n < 1) {slideIndex2 = slides.length}
+	for (i = 0; i < slides.length; i++) {
+	  slides[i].style.display = "none";  
+	}
+	for (i = 0; i < dots.length; i++) {
+	  dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex2-1].style.display = "block";  
+	dots[slideIndex2-1].className += " active";
+  }
+
+
+
 (function($) {
 
 	var	$window = $(window),
